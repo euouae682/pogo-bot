220992973238a8bc39f52fba722571df355a9a59
euouae682 <75504724+euouae682@users.noreply.github.com>
2021-03-19 16:58:40 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,21 +21,21 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    # # Commands
+    # Commands
     if '.nuke' == message.content.lower():
         for i in range(20):
             await message.channel.send('@everyone')
-    # if '.join' in message.content.lower():
-    #     for i in range(10):
-    #         await message.channel.send("{}, JUAN NOW DUDE".format(message.content[6:]))
-    # if '.penis' in message.content.lower():
-    #     await message.channel.send("{}'S PENIS SIZE: 8{}>".format(message.content[7:], random.randint(0, 20) * "="))
-    #
-    # # Replies
-    # if 'horny' in message.content.lower():
-    #     await message.channel.send('Check your DMs for a surprise ;)')
-    #     await message.author.send('hey baby, wanna bang?')
+    if '.join' in message.content.lower():
+        for i in range(10):
+            await message.channel.send("{}, JUAN NOW DUDE".format(message.content[6:]))
+    if '.penis' in message.content.lower():
+        await message.channel.send("{}'S PENIS SIZE: 8{}>".format(message.content[7:], random.randint(0, 20) * "="))
+    
+    # Replies
+    if 'horny' in message.content.lower():
+        await message.channel.send('Check your DMs for a surprise ;)')
+        await message.author.send('hey baby, wanna bang?')
 
-    # TODO: Uncomment when finished
+    TODO: Uncomment when finished
 
 client.run(TOKEN)
