4c9e12a57fffc8387edec896233af622da49bbf5
Timmie AYAYA <75507035+RedPandaGaminq@users.noreply.github.com>
2021-03-19 14:31:12 -0700

    Yes

---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+import discord
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(f'{client.user} is connected to the following guild:\n'
+          f'{guild.name} (id: {guild.id})')
+    await client.change_presence(activity=discord.Game(name='with your mom'))
+
+
+@client.event
+async def on_message(message):
+
+    # Commands
+    if '.nuke' == message.content.lower():
+        for i in range(20):
+            await message.channel.send('@everyone')
+    if '.join' in message.content.lower():
+        for i in range(10):
+            await message.channel.send("{}, JUAN NOW DUDE".format(message.content[6:]))
+    if '.random' == message.content.lower():
+        users = discord.utils.get(client.users)
+        await message.channel.send(users)
+
+    # Replies
+    if 'horny' in message.content.lower():
+        await message.channel.send('Check your DMs for a surprise ;)')
+        await message.author.send('hey baby, wanna bang?')
+
+
+client.run(TOKEN)
